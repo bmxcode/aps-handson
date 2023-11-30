9d9bf6a28fe0c3d2890eec484adb84b1a9a47c97
Peter Tos <tos@.adobe.com>
2023-11-30 17:00:34 +1100

    [syntax] updated to export block

--- blocks/syntax/syntax.js
+++ blocks/syntax/syntax.js
@@ -1,21 +1,20 @@
 import { loadCSS, loadScript } from "../../scripts/aem.js";
 
-const decorateSyntaxBlock = (el) => {
-  el.classList.forEach((className) => {
+const decorateSyntaxLanguage = (block) => {
+  block.classList.forEach((className) => {
     if (className !== "syntax" && className !== "block") {
-      el.getElementsByTagName("code")[0].classList.add(`language-${className}`);
+      block
+        .getElementsByTagName("code")[0]
+        .classList.add(`language-${className}`);
     }
   });
 };
 
 export default function decorate(block) {
-  const els = document.querySelectorAll(".syntax");
-  if (els.length) {
-    // load the prism assets
-    loadScript("/blocks/syntax/prism.js");
-    loadCSS("/blocks/syntax/prism.css");
+  // load the prism assets
+  loadScript("/blocks/syntax/prism.js");
+  loadCSS("/blocks/syntax/prism.css");
 
-    // process each syntax block
-    els.forEach(decorateSyntaxBlock);
-  }
+  // process each syntax block
+  decorateSyntaxLanguage(block);
 }
