b4c29a399a58d4bfba2ae9382e0e94d68c639c27
Peter Tos <tos@.adobe.com>
2023-11-30 12:56:03 +1100

    [syntax] added new syntax block

---
+++ blocks/syntax/syntax.js
@@ -0,0 +1,19 @@
+import { loadCSS, loadScript } from "../../scripts/aem.js";
+
+const decorateSyntaxBlock = (el) => {
+  el.classList.forEach((className) => {
+    if (className !== "syntax" && className !== "block") {
+      el.getElementsByTagName("code")[0].classList.add(`language-${className}`);
+    }
+  });
+};
+
+const els = document.querySelectorAll(".syntax");
+if (els.length) {
+  // load the prism assets
+  loadScript("/blocks/syntax/prism.js");
+  loadCSS("/blocks/syntax/prism.css");
+
+  // process each syntax block
+  els.forEach(decorateSyntaxBlock);
+}
