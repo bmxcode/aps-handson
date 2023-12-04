0f84b4e3e361dd1275324ff2249988327a20e5f5
Peter Tos <tos@.adobe.com>
2023-12-04 17:41:12 +1100

    syntax code and favicon

--- blocks/syntax/syntax.js
+++ blocks/syntax/syntax.js
@@ -1,13 +1,13 @@
 import { loadCSS, loadScript } from "../../scripts/aem.js";
 
 const decorateSyntaxLanguage = (block) => {
-  block.classList.forEach((className) => {
-    if (className !== "syntax" && className !== "block") {
-      block
-        .getElementsByTagName("code")[0]
-        .classList.add(`language-${className}`);
-    }
-  });
+  // classes are applied as "<blockName> <extraClass (|extraClass)> block"
+  const syntaxLang = block.classList[1];
+  if (syntaxLang !== "block") {
+    block
+      .getElementsByTagName("code")[0]
+      .classList.add(`language-${syntaxLang}`);
+  }
 };
 
 export default function decorate(block) {
